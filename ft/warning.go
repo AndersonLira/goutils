d270f87873c7cf59219bd4c832a0a76460e02c25
Anderson Lira <andersonliralink@gmail.com>
2018-07-31 09:35:22 -0300

    Warning receiving slice of interface

--- ft/warning.go
+++ ft/warning.go
@@ -3,11 +3,13 @@ package ft
 import "fmt"
 
 //Warning display a highlighted message on terminal
-func Warning(message interface{}) {
+func Warning(message ...interface{}) {
 	fmt.Println(ClrY, "==========================================================")
 	fmt.Println(ClrY, "=                                                        =")
 	fmt.Println(ClrY, "=                                                        =")
-	fmt.Println(Clr0, "=", message)
+	for _, it := range message {
+		fmt.Println(Clr0, "=", it)
+	}
 	fmt.Println(ClrY, "=                                                        =")
 	fmt.Println(ClrY, "=                                                        =")
 	fmt.Println(ClrY, "==========================================================")
