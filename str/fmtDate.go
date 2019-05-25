a5d25927db4a94fe0a28ec7dbb862333468f8f8f
Anderson Lira <andersonliralink@gmail.com>
2019-05-25 09:12:23 +0100

    Problem with ReplaceAll fixed. It was changed to Replace

--- str/fmtDate.go
+++ str/fmtDate.go
@@ -4,18 +4,18 @@ import (
 	"strings"
 	"time"
 )
-//FmtDate formats date with basic java pattern instead golang pattern
-func FmtDate(format string, t time.Time) string{
-    //2006-01-02T15:04:05Z07:00
-	newFormat := strings.ReplaceAll(format,"dd","02")
-	newFormat = strings.ReplaceAll(newFormat,"MMM","Jan")
-	newFormat = strings.ReplaceAll(newFormat,"MM","01")
-	newFormat = strings.ReplaceAll(newFormat,"yyyy","2006")
-	newFormat = strings.ReplaceAll(newFormat,"yy","06")
-	newFormat = strings.ReplaceAll(newFormat,"HH","15")
-	newFormat = strings.ReplaceAll(newFormat,"mm","04")
-	newFormat = strings.ReplaceAll(newFormat,"ss","05")
 
-	
+//FmtDate formats date with basic java pattern instead golang pattern
+func FmtDate(format string, t time.Time) string {
+	//2006-01-02T15:04:05Z07:00
+	newFormat := strings.Replace(format, "dd", "02", -1)
+	newFormat = strings.Replace(newFormat, "MMM", "Jan", -1)
+	newFormat = strings.Replace(newFormat, "MM", "01", -1)
+	newFormat = strings.Replace(newFormat, "yyyy", "2006", -1)
+	newFormat = strings.Replace(newFormat, "yy", "06", -1)
+	newFormat = strings.Replace(newFormat, "HH", "15", -1)
+	newFormat = strings.Replace(newFormat, "mm", "04", -1)
+	newFormat = strings.Replace(newFormat, "ss", "05", -1)
+
 	return t.Format(newFormat)
 }
