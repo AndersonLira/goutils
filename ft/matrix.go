94c27275bfc42829afb87b13fdb5d8824e823c16
Anderson Lira <anderson.lira@celfocus.com>
2021-05-30 08:25:10 +0100

    Fix color

--- ft/matrix.go
+++ ft/matrix.go
@@ -19,6 +19,7 @@ type Matrix struct {
 func (m Matrix) Println(s string) {
 	s = strPad(s, m.Width, " ", "RIGHT")[0 : m.Width-3]
 	fmt.Print(m.TextColor)
+	fmt.Print(NONE)
 	fmt.Println(fmt.Sprintf("%s %s%s", string(m.Separator), s, string(m.Separator)))
 }
 
