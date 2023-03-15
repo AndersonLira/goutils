631c09df01b09cc4dccce45ee728f3059c38180d
Anderson Lira (CTW) <Anderson.Lira@ctw.bmwgroup.com>
2023-03-15 10:08:28 +0000

    feat: master | test fixed

--- watcher_test/watcherFile_test.go
+++ watcher_test/watcherFile_test.go
@@ -1,32 +1,29 @@
 package watcher_test
 
 import (
-	"os"
 	"testing"
 
-	"github.com/andersonlira/goutils/watcher"
 	"github.com/andersonlira/goutils/io"
+	"github.com/andersonlira/goutils/watcher"
 )
 
-func TestStart(t *testing.T){
-	paths := []string {"./"}
-	wf := watcher.WatcherFile{Paths:paths}
-
+func TestStart(t *testing.T) {
+	paths := []string{"./"}
+	wf := watcher.WatcherFile{Paths: paths}
 
 	wf.Start()
-	io.WriteFile("./test.txt","content")
-	go io.WriteFile("./test.txt","content")
+	io.WriteFile("./test.txt", "content")
+	go io.WriteFile("./test.txt", "content")
 
 	fileChanged := <-wf.FileChanged
-	if fileChanged != "test.txt" {
-		t.Errorf("File changed should be test.txt but %s",fileChanged)
+	if fileChanged != "./test.txt" {
+		t.Errorf("File changed should be ./test.txt but %s", fileChanged)
 	}
 	fileChanged = <-wf.FileChanged
-	if fileChanged != "test.txt" {
-		t.Errorf("File changed should be test.txt but %s",fileChanged)
+	if fileChanged != "./test.txt" {
+		t.Errorf("File changed should be ./test.txt but %s", fileChanged)
 	}
 
-	os.Remove("./test.txt")
+	//os.Remove("./test.txt")
 
-	
 }
